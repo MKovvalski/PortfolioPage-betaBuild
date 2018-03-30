3ddf155075a38c14aec965c0d1fabddf8c14d123
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-03-30 15:46:50 +0200

    modeling Projects

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const arrayOfLinks = document.querySelectorAll(".menu-list li");
 
+function sizing (array, action, width) {
+    for (var i = 0; i < array.length; i++) {
+        arrayOfLinks[i].addEventListener(action, function () {
+            this.lastElementChild.style.width = width;
+        })
+    }
+}
 
+sizing(arrayOfLinks, "mouseover", "100%");
+sizing(arrayOfLinks, "mouseout", "0%");
 
 });
