25a5f375962a597a3ea54a47bb3a99d276e574b4
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-04-06 14:01:33 +0200

    modeling footer animation

--- js/main.js
+++ js/main.js
@@ -13,4 +13,24 @@ function sizing (array, action, width) {
 sizing(arrayOfLinks, "mouseover", "100%");
 sizing(arrayOfLinks, "mouseout", "0%");
 
+const gmail = document.querySelector(".gmail");
+const mailExpander = document.querySelector(".mail-expander");
+
+gmail.addEventListener("click", function () {
+    switch (this.id) {
+        case "none":
+            this.id = "move-left";
+            mailExpander.style.display = "block";
+            mailExpander.id = "expand";
+            console.log(mailExpander.id);
+            break;
+        case "move-left":
+            this.id = "move-right";
+            break;
+        case "move-right":
+            this.id = "move-left";
+            break;
+    }
+});
+
 });
