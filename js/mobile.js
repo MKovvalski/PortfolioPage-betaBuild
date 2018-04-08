cefacfec2cc18fe6ceb9d3a844fce2a530129410
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-04-08 16:15:31 +0200

    finished styling burger animation

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const first = document.querySelector(".first");
-    const second = document.querySelector(".second");
+    const second = document.querySelector(".menu-burger-line-wrapper");
+    const smallFirst = document.querySelector(".small-first");
+    const smallSecond = document.querySelector(".small-second");
     const third = document.querySelector(".third");
     const hamburgerMenu = document.querySelector(".menu-hamburger");
 
@@ -9,10 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
         switch (third.id) {
             case "rotate-right":
               first.id = "rotate-counter-left";
+              second.id = "move-down";
+              smallFirst.id = "rotate-counter-downward-right";
+              smallSecond.id = "rotate-counter-downward-left";
               third.id = "rotate-counter-right";
               break;
             default:
                 first.id = "rotate-left";
+                second.id = "move-up";
+                smallFirst.id = "rotate-downward-right";
+                smallSecond.id = "rotate-downward-left";
                 third.id = "rotate-right";
         }
     })
