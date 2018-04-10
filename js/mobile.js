14e53547f45931f4201bb3aa672a915dcf500f65
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2018-04-10 17:53:59 +0200

    added smoothscrolling for mobile version

--- js/mobile.js
+++ js/mobile.js
@@ -26,7 +26,49 @@ document.addEventListener("DOMContentLoaded", function () {
                 third.id = "rotate-right";
                 mainNavMobile.id = "display-block"
         }
-    })
+    });
 
+    // mobile header smooth scrolling ------ //
 
+    const mobileHeaderAbout = document.querySelector(".header-mobile-about");
+    const mobileHeaderSkills = document.querySelector(".header-mobile-skills");
+    const mobileHeaderProjects = document.querySelector(".header-mobile-projects");
+    const mobileHeaderContacts = document.querySelector(".header-mobile-contact");
+
+    // disclaimer - I did not write the smoothscroll - got it form stackoverflow
+
+    window.smoothScroll = function(target) {
+        var scrollContainer = target;
+        do { //find scroll container
+            scrollContainer = scrollContainer.parentNode;
+            if (!scrollContainer) return;
+            scrollContainer.scrollTop += 1;
+        } while (scrollContainer.scrollTop === 0);
+
+        var targetY = 0;
+        do { //find the top of target relatively to the container
+            if (target === scrollContainer) break;
+            targetY += target.offsetTop;
+        } while (target = target.offsetParent);
+
+        scroll = function(c, a, b, i) {
+            i++; if (i > 30) return;
+            c.scrollTop = a + (b - a) / 30 * i;
+            setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        };
+        // start scrolling
+        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    };
+
+    function mobileScrolling (element, scrollElement) {
+        element.addEventListener("click", function () {
+            smoothScroll(document.getElementById(scrollElement));
+        })
+    }
+
+    mobileScrolling(mobileHeaderAbout, 'about');
+    mobileScrolling(mobileHeaderSkills, 'skills');
+    mobileScrolling(mobileHeaderProjects, 'projects');
+    mobileScrolling(mobileHeaderContacts, 'contact');
+    
 });
